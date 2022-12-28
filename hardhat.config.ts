016e0d8cb421b7ee14d731bb18f1873761f9a8cb
Alex <alextroshkin@outlook.com>
2022-12-28 21:35:41 +0300

    - add hardhat network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,21 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-mainnet.g.alchemy.com/v2/vrcOlLGJbCrICwHZUJViWQZUAbWx0Mxc",
+        blockNumber: 15890000,
+      },
+      accounts: {
+        mnemonic: "deliver outdoor lawsuit jealous neck humble average bundle antenna among answer access",
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+        passphrase: "",
+      },
+    }
+  }
 };
 
 export default config;
